d73866c2b7809c8368a2b5255e847004a8830fd3
vego-bball <80690781+vego-bball@users.noreply.github.com>
2021-08-08 11:16:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//https://teachablemachine.withgoogle.com/models/i9ORyjagE/
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "jpeg",
+    jpeg_quality: 100
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src ="'+data_uri+'">';
+    });
+}
+
